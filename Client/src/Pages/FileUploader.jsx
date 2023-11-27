0ad76536515f72c976ab82535ebb8cc5b2e4623c
dev-kev-coder <recodedkevdev@gmail.com>
2023-11-26 20:14:36 -0800

    abstracted away logic into a hook.

--- Client/src/Pages/FileUploader.jsx
+++ Client/src/Pages/FileUploader.jsx
@@ -1,134 +1,35 @@
 import React, { useState } from 'react';
-
-const updateFilesToUploadState = (
-  usersFiles,
-  filesToUpload,
-  setStateCallback,
-) => {
-  for (const file of usersFiles) {
-    if (filesToUpload[file.name]) {
-      const uploadCopy = confirmWithUser();
-
-      if (uploadCopy) {
-        let copyAdded = false;
-        let copyVersionNum = 0;
-
-        while (!copyAdded) {
-          let fileCopyName = `${file.name} - (${copyVersionNum})`;
-
-          const fileCopy = new File([file], fileCopyName, {
-            type: file.type,
-            lastModified: file.lastModified,
-          });
-
-          copyAdded = addFile(fileCopy, filesToUpload);
-          copyVersionNum++;
-        }
-      }
-    } else {
-      const fileAdded = addFile(file, filesToUpload);
-
-      if (!fileAdded) alert('Something went wrong');
-    }
-
-    setStateCallback(filesToUpload);
-  }
-};
-const calculateFileSize = (fileSizeInBytes) => {
-  const units = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-
-  let unitIndex = 0;
-
-  let fileSize = fileSizeInBytes;
-
-  while (fileSize >= 1024 && unitIndex < units.length - 1) {
-    fileSize /= 1024;
-    unitIndex++;
-  }
-
-  return `${fileSize.toFixed(2)} ${units[unitIndex]}`;
-};
-
-const formatToMonthDayYear = (datetime) => {
-  const date = new Date(datetime);
-  return date.toLocaleDateString('en-US', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric',
-  });
-};
-
-const confirmWithUser = (callback) => {
-  if (!callback)
-    return confirm('File Name exists already would you like to upload a copy?');
-  else return notification();
-};
-
-const addFile = (file, files) => {
-  const { name, size, lastModified } = file;
-
-  if (files[name]) return false;
-
-  files[name] = {
-    payload: file,
-    name,
-    size: calculateFileSize(size),
-    lastModified: formatToMonthDayYear(lastModified),
-  };
-
-  return true;
-};
+import useFileUploader from '../hooks/useFileUploader';
 
 const FileUploaderContainer = ({ notificationCallback }) => {
-  const [filesToUpload, setFilesUploaded] = useState({});
+  const { filesToUpload, handleChange, handleDragOver, handleDrop } =
+    useFileUploader();
 
-  console.log(filesToUpload);
-
-  const handleChange = (e) => {
-    const newFiles = e.target.files;
-
-    const updatedFilesToUpload = { ...filesToUpload };
-
-    updateFilesToUploadState(newFiles, updatedFilesToUpload, setFilesUploaded);
-  };
-
-  const handleDragOver = (event) => {
-    event.preventDefault();
-  };
-
-  const handleDrop = (event) => {
-    event.preventDefault();
-
-    const newFiles = event.dataTransfer.files;
-
-    const updatedFilesToUpload = { ...filesToUpload };
-
-    updateFilesToUploadState(newFiles, updatedFilesToUpload, setFilesUploaded);
-  };
+  console.log('Current Files to upload: ', filesToUpload);
 
   return (
     <div>
-      {/* <label htmlFor="file">File</label>
-      <input
-        id="fileInput"
-        name="file"
-        type="file"
-        multiple
-        onChange={handleChange}
-      /> */}
-
       <div
         onDragOver={handleDragOver}
         onDrop={handleDrop}
         style={{
+          backgroundColor: 'maroon',
           border: '2px dashed black',
-          padding: '20px',
+          height: '300px',
           width: '300px',
           margin: 'auto',
         }}
       >
-        <input type="file" onChange={handleChange} multiple />
-        <p>Drag and drop files here, or click to select files</p>
+        <input
+          type="file"
+          onChange={handleChange}
+          style={{
+            opacity: 0,
+            width: '100%',
+            height: '100%',
+          }}
+          multiple
+        />
       </div>
     </div>
   );
---
+++ Client/src/hooks/useFileUploader.js
@@ -0,0 +1,112 @@
+import { useState } from 'react';
+
+const updateFilesToUploadState = (
+  usersFiles,
+  filesToUpload,
+  setStateCallback,
+) => {
+  for (const file of usersFiles) {
+    if (filesToUpload[file.name]) {
+      const uploadCopy = confirmWithUser();
+
+      if (uploadCopy) {
+        let copyAdded = false;
+        let copyVersionNum = 0;
+
+        while (!copyAdded) {
+          let fileCopyName = `${file.name} - (${copyVersionNum})`;
+
+          const fileCopy = new File([file], fileCopyName, {
+            type: file.type,
+            lastModified: file.lastModified,
+          });
+
+          copyAdded = addFile(fileCopy, filesToUpload);
+          copyVersionNum++;
+        }
+      }
+    } else {
+      const fileAdded = addFile(file, filesToUpload);
+
+      if (!fileAdded) alert('Something went wrong');
+    }
+
+    setStateCallback(filesToUpload);
+  }
+};
+const calculateFileSize = (fileSizeInBytes) => {
+  const units = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+
+  let unitIndex = 0;
+
+  let fileSize = fileSizeInBytes;
+
+  while (fileSize >= 1024 && unitIndex < units.length - 1) {
+    fileSize /= 1024;
+    unitIndex++;
+  }
+
+  return `${fileSize.toFixed(2)} ${units[unitIndex]}`;
+};
+
+const formatToMonthDayYear = (datetime) => {
+  const date = new Date(datetime);
+  return date.toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  });
+};
+
+const confirmWithUser = (callback) => {
+  if (!callback)
+    return confirm('File Name exists already would you like to upload a copy?');
+  else return notification();
+};
+
+const addFile = (file, files) => {
+  const { name, size, lastModified } = file;
+
+  if (files[name]) return false;
+
+  files[name] = {
+    payload: file,
+    name,
+    size: calculateFileSize(size),
+    lastModified: formatToMonthDayYear(lastModified),
+  };
+
+  return true;
+};
+export default () => {
+  const [filesToUpload, setFilesUploaded] = useState({});
+
+  const handleChange = (e) => {
+    const newFiles = e.target.files;
+
+    const updatedFilesToUpload = { ...filesToUpload };
+
+    updateFilesToUploadState(newFiles, updatedFilesToUpload, setFilesUploaded);
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+
+    const newFiles = event.dataTransfer.files;
+
+    const updatedFilesToUpload = { ...filesToUpload };
+
+    updateFilesToUploadState(newFiles, updatedFilesToUpload, setFilesUploaded);
+  };
+
+  return {
+    filesToUpload,
+    handleFilesChange: handleChange,
+    handelFilesDragOver: handleDragOver,
+    handleFilesDrop: handleDrop,
+  };
+};
