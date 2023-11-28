83242f8e03d50f2a7f2ac928df849b9fc01900a0
Kevin Albarran <recodedkevdev@gmail.com>
2023-11-28 00:29:23 -0800

    added a new controller

---
+++ Server/Controllers/FilesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpPost]
+        [Route("UploadFiles")]
+        public ActionResult UploadFiles()
+        {
+
+            var temp = "stop here";
+
+            return Ok("Post route hit");
+        }
+    }
+}
