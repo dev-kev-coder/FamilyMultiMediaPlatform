fd044e7ea4956b1b44b91180252d43d4f0fd6875
Kevin Albarran <recodedkevdev@gmail.com>
2024-07-07 23:52:43 -0700

    testing out svgs

--- Client/src/shared/components/Folder/Folder.jsx
+++ Client/src/shared/components/Folder/Folder.jsx
@@ -5,8 +5,50 @@ import UploadUI from '@sharedComponents/Uploader/UploadUI';
 const Folder = ({ propName }) => {
   return (
     <>
-      <UploadUI>
-        <div>folder</div>
+      <UploadUI id={'id'}>
+        {/* <div>folder</div> */}
+        {/* <!-- Simple Folder Icon with Tab --> */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="100"
+          height="100"
+          viewBox="0 0 24 24"
+        >
+          <rect
+            x="2"
+            y="6"
+            width="20"
+            height="14"
+            rx="2"
+            ry="2"
+            fill="#FFD700"
+          />
+          <rect
+            x="2"
+            y="4"
+            width="10"
+            height="4"
+            rx="1"
+            ry="1"
+            fill="#FFD700"
+          />
+          <rect
+            x="2"
+            y="4"
+            width="10"
+            height="4"
+            rx="1"
+            ry="1"
+            fill="rgba(0,0,0,0.1)"
+          />
+        </svg>
+
+        <svg width="100" height="100" viewBox="0 0 24 24">
+          <path
+            d="M2 4 H10 A1 1 0 0 1 11 5 V7 A1 1 0 0 1 10 8 H2 Z"
+            fill="#FFD700"
+          />
+        </svg>
       </UploadUI>
     </>
   );
