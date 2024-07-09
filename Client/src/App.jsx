78df382956cce74d88adc9216ee92b56a48782b0
dev-kev-coder <recodedkevdev@gmail.com>
2024-07-08 21:58:03 -0700

    started to flesh out more styling for UI

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,70 @@
 import Folder from '@sharedComponents/Folder/Folder';
 import React, { useState } from 'react';
-
+import FoldersAndFilesContainer from './components/FoldersAndFilesContainer';
+import './app.css';
 const App = () => {
   return (
-    <section>
-      <h1>Building out folder UI to be almost like OneDrive</h1>
-      <Folder />
-    </section>
+    <>
+      <header
+        style={{
+          backgroundColor: '#FFC857',
+          height: '3em',
+        }}
+      >
+        This will have header info
+      </header>
+
+      <section
+        style={{
+          padding: '1.5em',
+        }}
+      >
+        <FoldersAndFilesContainer>
+          <div
+            style={{
+              position: 'sticky',
+              top: '0',
+              backgroundColor: '#2C2C2C',
+              height: '4.0em',
+            }}
+          >
+            Header for table here
+          </div>
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+          <Folder />
+        </FoldersAndFilesContainer>
+      </section>
+    </>
   );
 };
 
