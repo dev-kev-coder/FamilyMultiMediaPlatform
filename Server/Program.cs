726383591f0f19110d8a15195cbc15c14cd877ec
Kevin Albarran <kalbarran@inland-engineering.com>
2024-09-09 22:31:16 -0700

    small opinionated changes

--- Server/Program.cs
+++ Server/Program.cs
@@ -26,8 +26,10 @@ namespace Server
             // Add services to the container.
             var app = builder.Build();
 
+            var isRunningInDevelopmentMode = app.Environment.IsDevelopment();
+
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
+            if (!isRunningInDevelopmentMode)
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
